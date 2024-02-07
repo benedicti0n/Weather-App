b91b94fa820f5c858a20ef8718a90ed80f39947a
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-02-07 21:44:22 +0530

    api key setup

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,14 @@
+function fetchWeather(){
+    const apiKey = "9b08b519d19dee71d87db4ef1d6e38ec";
+    const city = document.getElementById("city").value;
 
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+
+}
+
+
+const weatherIcon = document.getElementById("weatherIcon");
+const temperatureDiv = document.getElementById("temperatureDiv");
+const weatherInfo = document.getElementById("weatherInfo");
+const hourlyForecast = document.getElementById("hourlyForecast");
