15374556d8603286e734b78a8a51f77e4d954d68
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-02-07 21:52:37 +0530

    weather & forecast data function

--- dist/script.js
+++ dist/script.js
@@ -3,9 +3,45 @@ function fetchWeather(){
     const city = document.getElementById("city").value;
 
     const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+    const hourlyForecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+
+    async function fetchData(url){
+        try{
+            const response = await fetch(url);
+
+            if(!response.ok){
+                throw new error('Network response is not ok!');
+            }
+
+            const data = await response.json();
+            return data;
+        } catch{
+            console.error('Error fetching data: ', error);
+            throw new error('Error fetching data. Please try again.');
+        }
+    }
+
+    async function fetchCurrentWeather(){
+        try{
+            const data = await fetchData(weatherUrl);
+            displayWeather();
+        } catch(error){
+            alert(error.message);
+        }
+    }
+
+    async function fetchHourlyWeather(){
+        try{
+            const data = await fetchData(hourlyForecastUrl);
+            displayHourlyForecast(data.list);
+        } catch{
+            console.error(error.message);
+        }
+    }
 
 }
+
+
 
 
 const weatherIcon = document.getElementById("weatherIcon");
